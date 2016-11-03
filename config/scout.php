56e669a71ec58e83878061802e8777f4ff5f74e3
Reginaldo <reginaldo.junior696@gmail.com>
2016-11-03 18:47:33 -0200

    fix friends

--- config/scout.php
+++ config/scout.php
@@ -81,11 +81,11 @@ return [
     ],
     'tntsearch' => [
     'storage'  => storage_path(), //place where the index files will be stored
-    'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
+    'fuzziness' => env('TNTSEARCH_FUZZINESS', true),
     'fuzzy' => [
-        'prefix_length' => 2,
+        'prefix_length' => 5,
         'max_expansions' => 50,
-        'distance' => 2
+        'distance' => 5
     ],
 ],
 ];
